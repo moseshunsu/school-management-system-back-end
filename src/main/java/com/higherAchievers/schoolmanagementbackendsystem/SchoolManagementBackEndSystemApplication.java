fea6c7333413c4854d2b62c71a07bf37bc91be60
Moses Hunsu <hunsumoses7@gmail.com>
2023-06-17 10:58:33 +0100

    Initial commit

---
+++ src/main/java/com/higherAchievers/schoolmanagementbackendsystem/SchoolManagementBackEndSystemApplication.java
@@ -0,0 +1,13 @@
+package com.higherAchievers.schoolmanagementbackendsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementBackEndSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementBackEndSystemApplication.class, args);
+	}
+
+}
