5d339598242459ba11769c8f2821eb01ed2d8dac
Moses Hunsu <hunsumoses7@gmail.com>
2023-06-22 09:34:56 +0100

    Added the Student Controller Class

---
+++ src/main/java/com/higherAchievers/controller/StudController.java
@@ -0,0 +1,25 @@
+package com.higherAchievers.controller;
+
+import com.higherAchievers.dto.StudentRequest;
+import com.higherAchievers.service.StudServ;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/register-stud")
+public class StudController {
+
+    @Autowired
+    private StudServ studServ;
+
+    @PostMapping
+    public ResponseEntity<String> registerStudent(@RequestBody StudentRequest studentRequest) {
+        return ResponseEntity.ok(studServ.registerStudent(studentRequest));
+    }
+
+
+}
