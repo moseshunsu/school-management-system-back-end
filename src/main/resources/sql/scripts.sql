a9a599822c45e138146f3801d3bc2d112b31a07e
Moses Hunsu <hunsumoses7@gmail.com>
2023-06-23 00:25:44 +0100

    Added the SQL scripts for the creation of Teacher entity

--- src/main/resources/sql/scripts.sql
+++ src/main/resources/sql/scripts.sql
@@ -13,3 +13,25 @@ create table students (
     assignments varchar(250) default "",
     primary key (student_id)
 );
+
+
+create table teachers (
+	teacher_id bigint not null auto_increment,
+    gender varchar(7) not null,
+    first_name varchar(15) not null,
+    middle_name varchar(15) default "",
+    last_name varchar(15) not null,
+    email varchar(50) not null,
+    qualifications varchar(255) not null,
+    roles varchar(15) not null,
+    username varchar(15) not null,
+    `password` varchar(100) not null,
+    date_of_birth date not null,
+    address varchar(255) not null,
+    phone_number varchar(20) not null,
+    other_phone_number varchar(20) default "",
+    date_joined date,
+    last_modified_at datetime,
+    primary key (teacher_id)
+);
+
